8b526ab785e5d4f953a819c81b632103e5f467b3
Jay Sprout <jotasprout@gmail.com>
2018-06-06 07:08:19 -0400

    Added assets for sample

---
+++ GR_JS_object.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+var tableStart = '<table><tbody><tr><td colspan="3"><div class="pageTitle">Grand Rounds Videos</div></td></tr><tr><td colspan="3"><div class="instructions">Click any thumbnail to launch a video.</div></td></tr>';
+
+var yearRow = '<tr><td colspan="3"><div><img alt="2016 separation banner" src="files//upload/GrandRounds/GRyearBanner2016.png" /></div></td></tr>';
+ 
+var monthsRow = '<tr><td><div><a href="files//upload/GrandRounds/Roberts121416.wmv"><img title="Dying with Dignity by Kristin Kronsnoble on November 9, 2016" alt="" src="files//upload/GrandRounds/roberts121416.png" /></a></div></td><td><div><a href="files//upload/GrandRounds/EndOfLife_110916.wmv"><img title="Dying with Dignity by Kristin Kronsnoble on November 9, 2016" alt="" src="files//upload/GrandRounds/Kronsnoble.png" /></a></div></td><td><div><a href="files//upload/GrandRounds/Wagnor_101216.wmv"><img title="A Visit with the MPSC by Christine Wagner on October 12, 2016" alt="" src="files//upload/GrandRounds/Wagner.png" /></a></div></td></tr>';
+        
+var tableStop = '</tbody></table>';
+
+
+
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Scripts to generate gallery of thumbnails for videos in reverse chronological order.
 Updates table of image thumbnails with links to videos automatically
 when item added to array.
+
+Link to blog post
+
+https://jotascript.wordpress.com/2017/02/14/automate-the-tedious-stuff/
